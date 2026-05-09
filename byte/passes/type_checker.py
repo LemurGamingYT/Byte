3b204923a92289eefa4436ddaac9802be7fa896e
LemurGamingYT <alexdaltontubb@gmail.com>
2026-05-09 13:13:19 +0100

    File class

--- byte/passes/type_checker.py
+++ byte/passes/type_checker.py
@@ -3,9 +3,8 @@ from dataclasses import replace
 from logging import info
 from typing import cast
 
-from byte.intrinsics import intrinsic, IntrinsicCallContext
-from byte.llvm_extensions import llint, NULL
 from byte.passes import ByteCompilerPass
+from byte.classes import init_class
 from byte import ast
 
 
@@ -115,122 +114,9 @@ class TypeChecker(ByteCompilerPass):
     
     def visitProperty(self, node: ast.Property):
         return replace(node, type=self.visit(node.type))
-
-    def init_field(self, cls_type: ast.ClassType, i: int, field: ast.Property):
-        self_param = ast.Param(field.pos, cls_type, 'self')
-        @intrinsic(
-            None, field.type, [self_param], flags=ast.FunctionFlags(property=True),
-            override_name=f'{cls_type}.{field.name}'
-        )
-        def getter(ctx: IntrinsicCallContext):
-            struct = ctx.arg(0)
-            return ctx.builder.extract_value(struct, i, field.name)
-
-        field_get = self.visit(getattr(getter, 'ast_func'))
-
-        self_ref_param = ast.Param(field.pos, cls_type.reference(), 'self')
-        set_params = [self_ref_param, ast.Param(field.pos, field.type, 'value')]
-        @intrinsic(
-            None, self.file.type_map.get('nil'), set_params, flags=ast.FunctionFlags(method=True),
-            override_name=f'{cls_type}.set.{field.name}'
-        )
-        def setter(ctx: IntrinsicCallContext):
-            struct = ctx.arg(0)
-            value = ctx.arg(1)
-            field_ptr = ctx.builder.extract_ptr(struct, i, f'{field.name}.ptr')
-            ctx.builder.store(value, field_ptr)
-
-        field_set = self.visit(getattr(setter, 'ast_func'))
-        return field_get, field_set
-
-    def init_class(self, node: ast.Class):
-        cls_type = cast(ast.ClassType, self.file.type_map.get(node.name))
-
-        fields = [member for member in node.members if isinstance(member, ast.Property)]
-        constructor_params = [ast.Param(node.pos, member.type, member.name, ast.ParamFlags(copy=True)) for member in fields]
-        @intrinsic(
-            None, cls_type, constructor_params, flags=ast.FunctionFlags(static=True, method=True),
-            override_name=f'{cls_type}.new'
-        )
-        def constructor(ctx: IntrinsicCallContext):
-            struct_type = ctx.module.context.get_identified_type(node.name)
-            return ctx.builder.struct(struct_type, ctx.codegen_args, ctx.name)
-
-        new_constructor = self.visit(getattr(constructor, 'ast_func'))
-
-        destructor_params = [ast.Param(node.pos, cls_type.reference(), 'self')]
-        @intrinsic(
-            None, self.file.type_map.get('nil'), destructor_params, flags=ast.FunctionFlags(method=True),
-            override_name=f'{cls_type}.destroy'
-        )
-        def destructor(ctx: IntrinsicCallContext):
-            struct_ptr = ctx.arg(0)
-            for i, field in enumerate(fields):
-                field_destructor_name = f'{field.type}.destroy'
-                if not self.scope.symbol_table.has(field_destructor_name):
-                    continue
-
-                field_value = ctx.builder.extract_ptr(struct_ptr, i, f'{field.name}.ptr')
-                ctx.call(field_destructor_name, [ast.Arg(ctx.pos, field.type.reference(), field_value)])
-
-        destroy = self.visit(getattr(destructor, 'ast_func'))
-        
-        field_properties = []
-        for i, field in enumerate(fields):
-            field_properties.extend(self.init_field(cls_type, i, field))
-
-        string_type = self.file.type_map.get('string')
-        string_format = f'{node.name}(' + ', '.join(f'{field.name}=%.*s' for field in fields) + ')'
-        
-        @intrinsic(
-            None, string_type, [ast.Param(node.pos, cls_type, 'self')], flags=ast.FunctionFlags(method=True),
-            override_name=f'{cls_type}.to_string'
-        )
-        def cls_to_string(ctx: IntrinsicCallContext):
-            from llvmlite import ir
-            
-            asprintf = ctx.module.registry.get('asprintf')
-
-            struct = ctx.arg(0)
-            field_strs = []
-            for i, field in enumerate(fields):
-                field_value = ctx.builder.extract_value(struct, i, field.name)
-                field_str = ctx.call(f'{field.type}.to_string', [
-                    ast.Arg(ctx.pos, field.type, field_value)
-                ])
-                field_strs.append(field_str)
-
-            buf_addr = ctx.builder.alloca(ir.PointerType(ir.IntType(8)))
-            ctx.builder.store(NULL(), buf_addr)
-
-            fmt = ctx.module.try_get_global(
-                f'{node.name}_str_fmt', lambda: ctx.module.global_string(string_format, f'{node.name}_str_fmt')
-            )
-            fmt_ptr = ctx.builder.first_elem(fmt, 'fmt_ptr')
-
-            asprintf_args = [buf_addr, fmt_ptr]
-            for field_str in field_strs:
-                length = ctx.call('string.length', [ast.Arg(ctx.pos, ctx.file.type_map.get('string'), field_str)])
-                ptr = ctx.call('string.ptr', [ast.Arg(ctx.pos, ctx.file.type_map.get('string'), field_str)])
-                asprintf_args.append(length)
-                asprintf_args.append(ptr)
-
-            written = ctx.builder.call(asprintf, asprintf_args, 'written')
-            return ctx.call('string.new', [
-                ast.Arg(ctx.pos, ctx.file.type_map.get('pointer'), ctx.builder.load(buf_addr, 'buf')),
-                ast.Arg(ctx.pos, ctx.file.type_map.get('int'), written),
-                ast.Arg(ctx.pos, ctx.file.type_map.get('bool'), llint(1, 1))
-            ])
-
-        to_string = self.visit(getattr(cls_to_string, 'ast_func'))
-
-        return [new_constructor, destroy, to_string] + field_properties
     
     def visitClass(self, node: ast.Class):
-        field_types = [field.type for field in node.fields]
-        self.file.type_map.add(node.name, ast.ClassType(node.name, field_types))
-        
-        members = self.init_class(node)
+        members = init_class(node.pos, self.file, node.name, node.fields)
         for member in node.members:
             if isinstance(member, ast.Function):
                 member.extend_type = self.file.type_map.get(node.name)
--- byte/stdlib/fstream.py
+++ byte/stdlib/fstream.py
@@ -1,20 +1,15 @@
-from llvmlite import ir
-
-from byte.intrinsics import intrinsic, IntrinsicLib, IntrinsicCallContext
+from byte.intrinsics import IntrinsicLib
+from byte.classes import init_class
 from byte import ast
 
 
 class fstream(IntrinsicLib):
     def init(self):
         string_type = self.file.type_map.get('string')
-        File_type = self.file.type_map.add('File', ast.ClassType('File', [string_type]))
+        
+        class_members = init_class(ast.Position(), self.file, 'File', [
+            ast.Property(ast.Position(), string_type, 'filename')
+        ])
 
-        @intrinsic(
-            self, File_type, [ast.Param(ast.Position(), string_type, 'filename', flags=ast.ParamFlags(copy=True))],
-            flags=ast.FunctionFlags(static=True, method=True), override_name=f'{File_type}.new'
-        )
-        def File_new(ctx: IntrinsicCallContext):
-            filename = ctx.arg(0)
-
-            File = ctx.codegen.visit(File_type)
-            return ctx.builder.struct(File, [filename], ctx.name)
+        for member in class_members:
+            self.file.scope.symbol_table.add(ast.Symbol(member.name, self.file.type_map.get('function'), member))
